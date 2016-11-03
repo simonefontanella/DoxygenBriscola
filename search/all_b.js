ebc24d3db9092cb01813e65260f7f7079caef100
simonefontanella <fonta9879@gmail.com>
2016-11-03 19:38:27 +0100

    Add files via upload

---
+++ search/all_b.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['seme',['seme',['../class_fontanella_1_1_simone_1_1__5i_1_1_briscola_1_1_card.html#a5b099fd5db757dd88b5d3b402d97e638',1,'Fontanella::Simone::_5i::Briscola::Card']]],
+  ['spade',['Spade',['../class_fontanella_1_1_simone_1_1__5i_1_1_briscola_1_1_deck.html#a7f82994e5bc71f3c6ab33157e02554eaad3823d5046826424cb0b4f16a4daabcc',1,'Fontanella::Simone::_5i::Briscola::Deck']]],
+  ['startturn',['startTurn',['../class_fontanella_1_1_simone_1_1__5i_1_1_briscola_1_1_game_master.html#a43255b1c949556008efd7d22256fd294',1,'Fontanella::Simone::_5i::Briscola::GameMaster']]],
+  ['suit',['Suit',['../class_fontanella_1_1_simone_1_1__5i_1_1_briscola_1_1_deck.html#a7f82994e5bc71f3c6ab33157e02554ea',1,'Fontanella::Simone::_5i::Briscola::Deck']]]
+];
